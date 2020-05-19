325ae2dea977c68229250f78b666b7b937671a6f
Ruslan <rus2691@yandex.ru>
2020-05-19 22:41:17 +0300

    Второй

---
+++ main__1.js
@@ -0,0 +1,31 @@
+// let arr = ['plum.pnd', 'orange.jpg', 'apple.bmp'];
+
+// console.log(arr[0]);
+
+// var nota = 10;
+
+// console.log(nota);
+
+
+// let answer = +prompt("Если вам 18?", "Да")
+// console.log(typeof(answer))
+ 
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(incr++);
+console.log(decr--);
+console.log("2" === 2)
+
+
+
+
+
+
+
+
+
+
